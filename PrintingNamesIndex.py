391b2248e95fd7c05aef492d6704dd47e881deb3
Terrini <Haileykayy69@gmail.com>
2024-03-14 14:03:11 -0400

    PrintingNamesIndex Stable

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -2,3 +2,10 @@
 # Date: 3.12.24
 # Program: Printing names Index
 # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ["Hailey" , "Bella" , "katelyn" , "Maddie"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
